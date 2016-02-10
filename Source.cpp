18b915db2af7b406ce7522a689f7523930842b30
Diego Rodrigues <diegowebdevel@gmail.com>
2016-02-09 22:31:37 -0200

    Adicionado um arquivo fonte Alo do C++

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argc[])
+{
+	for(int i = 0; i <= 10; i++) 
+	{
+		std::cout << "Alo";
+		for(int j = 0; j <= 1; j++)
+			std::cout << " git." << std::endl;
+	}
+	
+	return 0;
+}
